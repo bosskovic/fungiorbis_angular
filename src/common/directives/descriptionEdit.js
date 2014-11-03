1fe3669a94e22a676a2759b5145d9d981f9191cd
Marko Bošković <bosskovic@gmail.com>
2014-11-03 03:56:07 +0100

    adds descriptionEdit directive

---
+++ src/common/directives/descriptionEdit.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('directives.descriptionEdit', [])
+
+  .directive('foDescriptionEdit', function () {
+
+    return {
+      templateUrl: 'common/directives/descriptionEdit.tpl.html',
+      restrict: 'E',
+      replace: false,
+      scope: { characteristic: '=' },
+      link: function (scope) {
+        scope.locales = [
+          { key: 'sr', title: 'srpski' },
+          { key: 'en', title: 'English'}
+        ];
+        scope.sections = [
+          { key: 'fruitingBody', title: 'Fruiting Body', locale: scope.locales[0] },
+          { key: 'flesh', title: 'Flesh', locale: scope.locales[0] },
+          { key: 'microscopy', title: 'Microscopy', locale: scope.locales[0] },
+          { key: 'chemistry', title: 'Chemistry', locale: scope.locales[0] },
+          { key: 'notes', title: 'Notes', locale: scope.locales[0] }
+        ];
+
+        var activeDescTab = scope.sections[0];
+
+        scope.active = function (tabName) {
+          return tabName === activeDescTab;
+        };
+
+        scope.activate = function (tabName) {
+          activeDescTab = tabName;
+        };
+
+      }
+    };
+  });

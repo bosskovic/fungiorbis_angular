df32a98d457664537f532f2b29797550d6872613
Marko Bošković <bosskovic@gmail.com>
2014-11-06 03:00:57 +0100

    adds placeholder to species typeahead

--- src/common/directives/speciesTypeahead.js
+++ src/common/directives/speciesTypeahead.js
@@ -8,7 +8,7 @@ angular.module('directives.speciesTypeahead', [])
       templateUrl: 'common/directives/speciesTypeahead.tpl.html',
       restrict: 'E',
       replace: false,
-      scope: { id: '=', fullName: '=' },
+      scope: { id: '=', fullName: '=', placeholder: '@' },
       link: function (scope) {
         scope.getSpecies = function (val) {
           return Species.$collection().$fetch({ filterTarget: 'name,genus', fields: 'id,fullName', filterValue: val}).$asPromise().then(function (response) {
@@ -18,6 +18,10 @@ angular.module('directives.speciesTypeahead', [])
         scope.select = function(item){
           scope.id = item.id;
         };
+
+        if (angular.isUndefined(scope.placeholder)){
+          scope.placeholder = 'Type in the species name';
+        }
       }
     };
   });

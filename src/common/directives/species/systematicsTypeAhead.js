06c2cf25f8807b433c0055caa7ad12b474339d81
Marko Bošković <bosskovic@gmail.com>
2014-11-30 03:07:49 +0100

    f14 - adds directive for species systematics type ahead

---
+++ src/common/directives/species/systematicsTypeAhead.js
@@ -0,0 +1,47 @@
+'use strict';
+
+angular.module('directives.systematicsTypeAhead', [])
+
+  .directive('foSystematicsTypeAhead', function (Species) {
+
+    return {
+      templateUrl: 'common/directives/species/systematicsTypeAhead.tpl.html',
+      restrict: 'E',
+      replace: true,
+      scope: { systematics: '=' },
+      link: function (scope) {
+        scope.systematicsCategory = 'genus';
+
+        scope.allSystematics = [
+          { key: 'fullName', value: 'vrsta' },
+          { key: 'genus', value: 'rod' },
+          { key: 'familia', value: 'familija' },
+          { key: 'ordo', value: 'red' },
+          { key: 'subclassis', value: 'podklasa' },
+          { key: 'classis', value: 'klasa' },
+          { key: 'subphylum', value: 'podfilum' },
+          { key: 'phylum', value: 'filum' }
+        ];
+
+        scope.$watch('systematicsCategory', function (systematicsCategory) {
+          scope.allSystematics.forEach(function(s){
+            if (s.key === systematicsCategory){
+              scope.systematicsCategoryLabel =  s.value;
+            }
+          });
+        });
+
+        scope.typeAheadProperties = {
+          icon: 'species',
+          key: 'fullTitle',
+          placeholder: '',
+          getData: function (val) {
+            return Species.systematicsValues(scope.systematicsCategory, {value: val})
+              .then(function (response) {
+                return response.data.systematics;
+              });
+          }
+        };
+      }
+    };
+  });
